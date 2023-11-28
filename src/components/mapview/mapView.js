7050624abedad91fd84e504fa4a78db44383499d
jimmyDev <jimmybuidev@gmail.com>
2023-11-28 08:36:49 -0800

    search function completede

--- src/components/mapview/mapView.js
+++ src/components/mapview/mapView.js
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Papa from 'papaparse';
 import { GoogleMap, useJsApiLoader, Marker, MarkerClusterer, InfoWindowF } from '@react-google-maps/api';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { useNavigate } from 'react-router-dom';
+import {Box, Flex} from '@chakra-ui/layout';
 
 function patchData(reports, restaurants) {
   let reportHashMap = new Map();
@@ -29,7 +29,7 @@ const center = {
 };
 
 const containerStyle = {
-  margin: '2vh',
+  marginLeft: '3vw',
   width: '90vw',
   height: '60vh'
 };
@@ -39,6 +39,7 @@ export const MapView = () => {
   const [totalRestaurants, setTotalRestaurants] = useState([]);
   const [reports, setReports] = useState([]);
   const navigate = useNavigate();
+  const [search, setSearch] = useState("");  
 
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
@@ -90,60 +91,113 @@ export const MapView = () => {
   const [activeMarker, setActiveMarker] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
 
+  const filteredRestaurants = totalRestaurants
+  .filter((restaurant) => {
+    return restaurant.properties.NAME.toLowerCase().includes(search.toLowerCase());
+  })
+
+  const handleRestaurantClick = (restaurant) => {
+    map.panTo({
+      lat: parseFloat(restaurant.properties.LATITUDE), 
+      lng: parseFloat(restaurant.properties.LONGITUDE)
+    });
+    map.setZoom(18);
+    setActiveMarker(restaurant);
+    setIsOpen(true);
+  }; 
+
   return isLoaded ? (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={11}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
+    <Flex 
+      position='relative'
+      flexDirection='column'
+      alignItems='center'
     >
-      <MarkerClusterer averageCenter enableRetinaIcons gridSize={60}>
-        {clusterer => 
-          totalRestaurants.map(restaurant => {
-            const {TRACKINGNUMBER, LATITUDE, LONGITUDE, NAME, PHYSICALADDRESS, reports} = restaurant.properties;
-            let hazardRating;
-            if(reports)
-              hazardRating=  Object.entries(reports)[0][1].HAZARDRATING;
-            else
-              hazardRating="none"
-            
-            return(
-              <Marker
-                key={TRACKINGNUMBER}
-                position={{
-                  lat: parseFloat(LATITUDE),
-                  lng: parseFloat(LONGITUDE)
-                }}
-                clusterer={clusterer}
-                onClick={() => {
-                  setActiveMarker(restaurant);
-                  setIsOpen(true);
-                }}
-              >
-                {isOpen && activeMarker === restaurant && (
-                  <InfoWindowF 
-                    onCloseClick={() => setIsOpen(false)}
+      <Box
+        mt={'3vh'}
+        mr={'40vw'}
+        bgColor='white'
+        zIndex='1'
+      >
+        <div className='mx-2 my-2'>
+          <input 
+            type="search" className="w-full"
+            value={search} 
+            onChange={(e) => setSearch(e.target.value)} 
+            placeholder=" Search for a restaurant"
+          />
+          <div style={{ maxHeight: '7vh', overflowY: 'scroll'}}
+            className="w-96">
+            {search !== "" && filteredRestaurants.map((restaurant, index) => (
+              <div key={index}>
+                  <button onClick={() => handleRestaurantClick(restaurant)}>{restaurant.properties.NAME} </button>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Box>
+     
+      <Box position='absolute' left={'2vw'} top={20} h='100%' w='100%'>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={11}
+        onLoad={onLoad}
+        onUnmount={onUnmount}
+        options={{
+          streetViewControl: false,
+          mapTypeControl: false,
+        }}
+      >
+          <MarkerClusterer averageCenter enableRetinaIcons gridSize={60}>
+            {clusterer => 
+              totalRestaurants.map(restaurant => {
+                const {TRACKINGNUMBER, LATITUDE, LONGITUDE, NAME, PHYSICALADDRESS, reports} = restaurant.properties;
+                let hazardRating;
+                if(reports)
+                  hazardRating=  Object.entries(reports)[0][1].HAZARDRATING;
+                else
+                  hazardRating="none"
+                
+                return(
+                  <Marker
+                    key={TRACKINGNUMBER}
+                    position={{
+                      lat: parseFloat(LATITUDE),
+                      lng: parseFloat(LONGITUDE)
+                    }}
+                    clusterer={clusterer}
+                    onClick={() => {
+                      setActiveMarker(restaurant);
+                      setIsOpen(true);
+                    }}
                   >
-                    <div>
-                    <h2 
-                      className="cursor-pointer underline font-bold"
-                      onClick={() => navigate(`/details/${TRACKINGNUMBER}`)}
-                    >
-                      {NAME}
-                    </h2>
-                      <p>Address: {PHYSICALADDRESS}</p>
-                      <p className='font-bold text-left'> 
-                        Hazard Rating: {hazardRating} </p>
-                    </div>
-                  </InfoWindowF>
-                )}
-              </Marker>
-            )
-          })
-        }
-      </MarkerClusterer>
-    </GoogleMap>
+                    {isOpen && activeMarker === restaurant && (
+                      <InfoWindowF 
+                        onCloseClick={() => setIsOpen(false)}
+                      >
+                        <div>
+                        <h2 
+                          className="cursor-pointer underline font-bold"
+                          onClick={() => navigate(`/details/${TRACKINGNUMBER}`)}
+                        >
+                          {NAME}
+                        </h2>
+                          <p>Address: {PHYSICALADDRESS}</p>
+                          <p className='font-bold text-left'> 
+                            Hazard Rating: {hazardRating} </p>
+                        </div>
+                      </InfoWindowF>
+                    )}
+                  </Marker>
+                )
+              })
+            }
+          </MarkerClusterer>
+        </GoogleMap>
+      </Box>
+     
+    </Flex>
+    
   ) : <></>
 }
 
